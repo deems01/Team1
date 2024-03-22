319520f942900f843819adabc6776e02b73bccab
veiko vilta <veikovilta@gmail.com>
2024-03-22 21:43:21 +0200

    first prototype of main page

--- PrjMainPage/MainPageForm.Designer.vb
+++ PrjMainPage/MainPageForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MainPageForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,235 @@ Partial Class MainPageForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.btnMinimize = New FontAwesome.Sharp.IconButton()
+        Me.btnMaximize = New FontAwesome.Sharp.IconButton()
+        Me.btnExit = New FontAwesome.Sharp.IconButton()
+        Me.IconButton3 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton4 = New FontAwesome.Sharp.IconButton()
+        Me.btnCategory = New FontAwesome.Sharp.IconButton()
+        Me.IconButton2 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.IconButton5 = New FontAwesome.Sharp.IconButton()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'btnMinimize
+        '
+        Me.btnMinimize.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnMinimize.BackColor = System.Drawing.Color.Transparent
+        Me.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnMinimize.IconChar = FontAwesome.Sharp.IconChar.WindowMinimize
+        Me.btnMinimize.IconColor = System.Drawing.Color.Black
+        Me.btnMinimize.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.btnMinimize.IconSize = 30
+        Me.btnMinimize.Location = New System.Drawing.Point(856, 2)
+        Me.btnMinimize.Name = "btnMinimize"
+        Me.btnMinimize.Size = New System.Drawing.Size(84, 23)
+        Me.btnMinimize.TabIndex = 1
+        Me.btnMinimize.UseVisualStyleBackColor = False
+        '
+        'btnMaximize
+        '
+        Me.btnMaximize.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnMaximize.BackColor = System.Drawing.Color.Transparent
+        Me.btnMaximize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnMaximize.IconChar = FontAwesome.Sharp.IconChar.WindowMaximize
+        Me.btnMaximize.IconColor = System.Drawing.Color.Black
+        Me.btnMaximize.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.btnMaximize.IconSize = 25
+        Me.btnMaximize.Location = New System.Drawing.Point(937, 2)
+        Me.btnMaximize.Name = "btnMaximize"
+        Me.btnMaximize.Size = New System.Drawing.Size(84, 23)
+        Me.btnMaximize.TabIndex = 2
+        Me.btnMaximize.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.BackColor = System.Drawing.Color.Transparent
+        Me.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnExit.IconChar = FontAwesome.Sharp.IconChar.Xmark
+        Me.btnExit.IconColor = System.Drawing.Color.Black
+        Me.btnExit.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.btnExit.IconSize = 25
+        Me.btnExit.Location = New System.Drawing.Point(1017, 2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(78, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'IconButton3
+        '
+        Me.IconButton3.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.IconButton3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.IconButton3.IconChar = FontAwesome.Sharp.IconChar.BarChart
+        Me.IconButton3.IconColor = System.Drawing.Color.Black
+        Me.IconButton3.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton3.IconSize = 80
+        Me.IconButton3.Location = New System.Drawing.Point(419, 3)
+        Me.IconButton3.Name = "IconButton3"
+        Me.IconButton3.Size = New System.Drawing.Size(202, 79)
+        Me.IconButton3.TabIndex = 6
+        Me.IconButton3.UseVisualStyleBackColor = False
+        '
+        'IconButton4
+        '
+        Me.IconButton4.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.IconButton4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.IconButton4.IconChar = FontAwesome.Sharp.IconChar.BarChart
+        Me.IconButton4.IconColor = System.Drawing.Color.Black
+        Me.IconButton4.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton4.IconSize = 80
+        Me.IconButton4.Location = New System.Drawing.Point(211, 3)
+        Me.IconButton4.Name = "IconButton4"
+        Me.IconButton4.Size = New System.Drawing.Size(202, 79)
+        Me.IconButton4.TabIndex = 7
+        Me.IconButton4.UseVisualStyleBackColor = False
+        '
+        'btnCategory
+        '
+        Me.btnCategory.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnCategory.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnCategory.IconChar = FontAwesome.Sharp.IconChar.BarChart
+        Me.btnCategory.IconColor = System.Drawing.Color.Black
+        Me.btnCategory.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.btnCategory.IconSize = 80
+        Me.btnCategory.Location = New System.Drawing.Point(3, 3)
+        Me.btnCategory.Name = "btnCategory"
+        Me.btnCategory.Size = New System.Drawing.Size(202, 79)
+        Me.btnCategory.TabIndex = 0
+        Me.btnCategory.UseVisualStyleBackColor = False
+        '
+        'IconButton2
+        '
+        Me.IconButton2.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.IconButton2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.IconButton2.IconChar = FontAwesome.Sharp.IconChar.BarChart
+        Me.IconButton2.IconColor = System.Drawing.Color.Black
+        Me.IconButton2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton2.IconSize = 80
+        Me.IconButton2.Location = New System.Drawing.Point(627, 3)
+        Me.IconButton2.Name = "IconButton2"
+        Me.IconButton2.Size = New System.Drawing.Size(202, 79)
+        Me.IconButton2.TabIndex = 5
+        Me.IconButton2.UseVisualStyleBackColor = False
+        '
+        'IconButton1
+        '
+        Me.IconButton1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.IconButton1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.BarChart
+        Me.IconButton1.IconColor = System.Drawing.Color.Black
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.IconSize = 80
+        Me.IconButton1.Location = New System.Drawing.Point(835, 3)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Size = New System.Drawing.Size(203, 79)
+        Me.IconButton1.TabIndex = 4
+        Me.IconButton1.UseVisualStyleBackColor = False
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 5
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.IconButton1, 4, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.IconButton2, 3, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnCategory, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.IconButton4, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.IconButton3, 2, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(30, 45)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 85.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1041, 85)
+        Me.TableLayoutPanel1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.TextBox2.Location = New System.Drawing.Point(18, 8)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(461, 29)
+        Me.TextBox2.TabIndex = 10
+        Me.TextBox2.Text = "Search"
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.Gainsboro
+        Me.Panel1.Controls.Add(Me.IconButton5)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(241, 280)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(560, 44)
+        Me.Panel1.TabIndex = 11
+        '
+        'IconButton5
+        '
+        Me.IconButton5.BackColor = System.Drawing.Color.White
+        Me.IconButton5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.IconButton5.Dock = System.Windows.Forms.DockStyle.Right
+        Me.IconButton5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.IconButton5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.IconButton5.IconChar = FontAwesome.Sharp.IconChar.Sistrix
+        Me.IconButton5.IconColor = System.Drawing.Color.Black
+        Me.IconButton5.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton5.Location = New System.Drawing.Point(488, 0)
+        Me.IconButton5.Name = "IconButton5"
+        Me.IconButton5.Size = New System.Drawing.Size(72, 44)
+        Me.IconButton5.TabIndex = 11
+        Me.IconButton5.UseVisualStyleBackColor = False
         '
         'MainPageForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.BackgroundImage = Global.PrjMainPage.My.Resources.Resources.wallpaperflare_com_wallpaper
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1096, 570)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnMaximize)
+        Me.Controls.Add(Me.btnMinimize)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "MainPageForm"
+        Me.Padding = New System.Windows.Forms.Padding(10)
         Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-
+    Friend WithEvents btnMinimize As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnMaximize As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnExit As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton3 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton4 As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnCategory As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton2 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents IconButton5 As FontAwesome.Sharp.IconButton
 End Class
--- PrjMainPage/MainPageForm.vb
+++ PrjMainPage/MainPageForm.vb
@@ -1,5 +1,95 @@
-﻿Public Class MainPageForm
+﻿
+Imports System.Runtime.InteropServices
+Imports System.Xml
+
+Public Class MainPageForm
+
+    ' Constants for mouse messages
+    Private Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Private Const HT_CAPTION As Integer = &H2
+
+    Public Sub New()
+        ' This call is required by the designer.'
+        InitializeComponent()
+        ' Add any initialization after the InitializeComponent() call.'
+        'Form'
+        'Me.Text = String.Empty
+        Me.ControlBox = False
+        Me.DoubleBuffered = True
+        Me.MaximizedBounds = Screen.PrimaryScreen.WorkingArea
+    End Sub
+
+    ' Import ReleaseCapture and SendMessage functions
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(hWnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+
+    Private Sub MainPageForm_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        If sender Is Nothing Then
+            Throw New ArgumentNullException(NameOf(sender))
+        End If
+        ' Check if the left mouse button is pressed
+        If e.Button = MouseButtons.Left Then
+            ' Call ReleaseCapture function to release mouse capture from the form
+            ReleaseCapture()
+            ' Send the WM_NCLBUTTONDOWN message to the form with HT_CAPTION parameter
+            SendMessage(Me.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0)
+        End If
+    End Sub
+
+    Private Sub btnCategory_MouseEnter(sender As Object, e As EventArgs)
+        ' Change the background color when the mouse enters the panel
+        btnCategory.BackColor = Color.Gold
+    End Sub
+
+    Private Sub btnCategory_MouseLeave(sender As Object, e As EventArgs)
+        ' Restore the original background color when the mouse leaves the panel
+        btnCategory.BackColor = Color.White
+    End Sub
+
+    Private Sub btnExit_MouseEnter(sender As Object, e As EventArgs) Handles btnExit.MouseEnter
+        ' Change the background color when the mouse enters the panel
+        btnExit.BackColor = Color.Red
+    End Sub
+
+    Private Sub btnExit_MouseLeave(sender As Object, e As EventArgs) Handles btnExit.MouseLeave
+        ' Restore the original background color when the mouse leaves the panel
+        btnExit.BackColor = Color.Transparent
+    End Sub
+
     Private Sub MainPageForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        btnMinimize.FlatStyle = FlatStyle.Flat
+        btnMinimize.FlatAppearance.BorderSize = 0
+
+        btnMaximize.FlatStyle = FlatStyle.Flat
+        btnMaximize.FlatAppearance.BorderSize = 0
+
+        btnExit.FlatStyle = FlatStyle.Flat
+        btnExit.FlatAppearance.BorderSize = 0
+
+    End Sub
+
+    'Remove transparent border in maximized state'
+
+
+    'Close-Maximize-Minimize'
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+    Private Sub btnMaximize_Click(sender As Object, e As EventArgs) Handles btnMaximize.Click
+        If WindowState = FormWindowState.Normal Then
+            WindowState = FormWindowState.Maximized
+        Else
+            WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub btnMinimize_Click(sender As Object, e As EventArgs) Handles btnMinimize.Click
+        WindowState = FormWindowState.Minimized
     End Sub
 End Class
